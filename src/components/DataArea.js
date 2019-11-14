2b4b3c682f7c037220955ed05eade3cfb788fbaf
Chris Flick <chrisflick@gmail.com>
2019-11-14 13:20:01 -0800

    Changed project to use functions instead of classes

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,105 +1,128 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import DataTable from "./DataTable";
 import Nav from "./Nav";
 import API from "../utils/API";
 import "../styles/DataArea.css";
 
-export default class DataArea extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [{}],
-      order: "descend",
-      filteredUsers: [{}],
-      headings: [
-        { name: "Image", width: "10%" },
-        { name: "Name", width: "10%" },
-        { name: "Phone", width: "20%" },
-        { name: "Email", width: "20%" },
-        { name: "DOB", width: "10%" }
-      ],
+export default function DataArea() {
 
-      handleSort: heading => {
+  const [state, setState] = useState({
+    users: [{}],
+    order: "descend",
+    filteredUsers: [{}],
+    headings: [
+      { name: "Image", width: "10%" },
+      { name: "Name", width: "10%" },
+      { name: "Phone", width: "20%" },
+      { name: "Email", width: "20%" },
+      { name: "DOB", width: "10%" }
+    ],
+  })
 
-        if (this.state.order === "descend") {
-          this.setState({
-            order: "ascend"
-          })
+  useEffect(() => {
+    API.getUsers().then(results => {
+      setState({
+        users: results.data.results,
+        order: state.order,
+        filteredUsers: results.data.results,
+        headings: state.headings
+      })
+    })
+  }, [])
+
+
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     users: [{}],
+  //     order: "descend",
+  //     filteredUsers: [{}],
+  //     headings: [
+  //       { name: "Image", width: "10%" },
+  //       { name: "Name", width: "10%" },
+  //       { name: "Phone", width: "20%" },
+  //       { name: "Email", width: "20%" },
+  //       { name: "DOB", width: "10%" }
+  //     ],
+
+  let handleSort = heading => {
+
+    if (state.order === "descend") {
+      setState({
+        order: "ascend"
+      })
+    } else {
+      setState({
+        order: "descend"
+      })
+    }
+
+    const compareFnc = (a, b) => {
+      if (state.order === "ascend") {
+        // account for missing values
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        }
+        // numerically
+        else if (heading === "name") {
+          return a[heading].first.localeCompare(b[heading].first);
         } else {
-          this.setState({
-            order: "descend"
-          })
+          return a[heading] - b[heading];
         }
-
-        const compareFnc = (a, b) => {
-          if (this.state.order === "ascend") {
-            // account for missing values
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            }
-            // numerically
-            else if (heading === "name") {
-              return a[heading].first.localeCompare(b[heading].first);
-            } else {
-              return a[heading] - b[heading];
-            }
-          } else {
-            // account for missing values
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            }
-            // numerically
-            else if (heading === "name") {
-              return b[heading].first.localeCompare(a[heading].first);
-            } else {
-              return b[heading] - a[heading];
-            }
-          }
-
+      } else {
+        // account for missing values
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
         }
-        const sortedUsers = this.state.filteredUsers.sort(compareFnc);
-        this.setState({ filteredUsers: sortedUsers });
-      },
-      handleSearchChange: event => {
-        console.log(event.target.value);
-        const filter = event.target.value;
-        const filteredList = this.state.users.filter(item => {
-          // merge data together, then see if user input is anywhere inside
-          let values = Object.values(item)
-            .join("")
-            .toLowerCase();
-          return values.indexOf(filter.toLowerCase()) !== -1;
-        });
-        this.setState({ filteredUsers: filteredList });
+        // numerically
+        else if (heading === "name") {
+          return b[heading].first.localeCompare(a[heading].first);
+        } else {
+          return b[heading] - a[heading];
+        }
       }
-    };
+
+    }
+    const sortedUsers = this.state.filteredUsers.sort(compareFnc);
+    setState({ filteredUsers: sortedUsers });
   }
 
-  componentDidMount() {
-    API.getUsers().then(results => {
-      this.setState({
-        users: results.data.results,
-        filteredUsers: results.data.results
-      });
+  let handleSearchChange = event => {
+    console.log(event.target.value);
+    const filter = event.target.value;
+    const filteredList = state.users.filter(item => {
+      // merge data together, then see if user input is anywhere inside
+      let values = Object.values(item)
+        .join("")
+        .toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    setState({ 
+      filteredUsers: filteredList,// Set the filtered Users
+      // Ensure that the state remains the same for other variables:
+      headings: state.headings,
+      users: state.users,
+      order: state.order
     });
   }
 
-  render() {
-    return (
-      <>
-        <Nav handleSearchChange={this.state.handleSearchChange} />
-        <div className="data-area">
-          <DataTable
-            headings={this.state.headings}
-            users={this.state.filteredUsers}
-            handleSort={this.state.handleSort}
-          />
-        </div>
-      </>
-    );
-  }
+
+
+
+  return (
+    <>
+      <Nav handleSearchChange={handleSearchChange} />
+      <div className="data-area">
+        <DataTable
+          headings={state.headings}
+          users={state.filteredUsers}
+          handleSort={handleSort}
+        />
+      </div>
+    </>
+  )
 }
